59ae2831f5ad7b62817d3c93267e62110ecda388
Andy Webber <andy@aligature.com>
2020-02-02 14:48:01 -0500

    Add support for all_photos hash database. Moved to sha1 checksum. Added real command line argument parsing.

--- flickr_upload.py
+++ flickr_upload.py
@@ -7,17 +7,33 @@ import sys
 import functools
 import hashlib
 from time import sleep
+import argparse
 
 LowerExtensions = ['.jpg', '.png', '.mov', '.jpeg', '.mp4', '.m4v']
 Extensions = LowerExtensions + [ext.upper() for ext in LowerExtensions]
 
 RetryLimit = 50
 
+parser = argparse.ArgumentParser()
+parser.add_argument('-k', '--api-key')
+parser.add_argument('-s', '--api-secret')
+parser.add_argument('-d', '--directory')
+parser.add_argument('-b', '--backup-directory')
+parser.add_argument('-t', '--tags')
+parser.add_argument('-r', '--hash-repo')
+
+options = parser.parse_args()
+if isinstance(options, tuple):
+    args = options[0]
+else:
+    args = options
+del options
+
 hashes = {}
 
 def file_hash(file_path):
     with open(file_path, 'rb') as f:
-        return hashlib.md5(f.read()).hexdigest()
+        return hashlib.sha1(f.read()).hexdigest()
 
 def upload_file(name, file, tags = ''):
     retry_count = 0
@@ -37,12 +53,12 @@ def upload_dir(directory, backup, tags):
     for name in sorted(files):
         if os.path.splitext(name)[1] in Extensions:
             whole_file = os.path.join(root, name)
-            md5 = file_hash(whole_file)
-            if md5 in hashes:
-                print 'skipping %s, duplicate of %s' % (name, hashes[md5])
+            this_hash = file_hash(whole_file)
+            if this_hash in hashes:
+                print 'skipping %s, duplicate of %s' % (name, hashes[this_hash])
                 os.rename(whole_file, os.path.join(backup, name))
             else:
-                hashes[md5] = name
+                hashes[this_hash] = name
                 if upload_file(name, whole_file, tags):
                     print 'moving %s' % name
                     os.rename(whole_file, os.path.join(backup, name))
@@ -51,20 +67,25 @@ def scan_backup(directory):
     root, dirs, files = os.walk(directory).next()
     for name in sorted(files):
         whole_file = os.path.join(root, name)
-        md5 = file_hash(whole_file)
-        hashes[md5] = name
+        this_hash = file_hash(whole_file)
+        hashes[this_hash] = name
 
-api_key = sys.argv[1]
-api_secret = sys.argv[2]
+def load_hash_repo(filename):
+    for line in open(filename):
+        line = line.strip()
+        splits = line.split(' ')
+        hashes[splits[0]] = splits[-1]
 
-flickr = flickrapi.FlickrAPI(api_key, api_secret)
+if not args.api_key or not args.api_secret:
+    print 'must include api key and secret'
+    exit(1)
+
+flickr = flickrapi.FlickrAPI(args.api_key, args.api_secret)
 flickr.authenticate_via_browser(perms='write')
 
-directory = sys.argv[3]
-backup = sys.argv[4]
-tags = ''
-if len(sys.argv) > 5:
-    tags = sys.argv[5]
+directory = args.directory
+backup = args.backup_directory
+tags = args.tags if args.tags else ''
 
 if directory == '' or backup == '':
     print 'include upload and backup directories'
@@ -73,4 +94,6 @@ if directory == '' or backup == '':
 if not os.path.exists(backup):
     os.makedirs(backup)
 scan_backup(backup)
+if args.hash_repo:
+    load_hash_repo(args.hash_repo)
 upload_dir(directory, backup, tags)
