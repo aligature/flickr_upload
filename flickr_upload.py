4d035744224f1ee10adbe7854824d536e3b7b5b5
Andy Webber <andy@aligature.com>
2024-11-23 15:29:03 -0500

    Sync from latest. Looks like python 3 support.

--- flickr_upload.py
+++ flickr_upload.py
@@ -8,6 +8,8 @@ import functools
 import hashlib
 from time import sleep
 import argparse
+import logging
+import webbrowser
 
 LowerExtensions = ['.jpg', '.png', '.mov', '.jpeg', '.mp4', '.m4v']
 Extensions = LowerExtensions + [ext.upper() for ext in LowerExtensions]
@@ -21,6 +23,7 @@ parser.add_argument('-d', '--directory')
 parser.add_argument('-b', '--backup-directory')
 parser.add_argument('-t', '--tags')
 parser.add_argument('-r', '--hash-repo')
+parser.add_argument('--log-level')
 
 options = parser.parse_args()
 if isinstance(options, tuple):
@@ -28,6 +31,10 @@ if isinstance(options, tuple):
 else:
     args = options
 del options
+
+if args.log_level:
+    print(args.log_level)
+    logging.basicConfig(level = args.log_level)
 
 hashes = {}
 
@@ -38,37 +45,39 @@ def file_hash(file_path):
 def upload_file(name, file, tags = ''):
     retry_count = 0
     while retry_count < RetryLimit:
-        print 'uploading %s (%s)' % (name, retry_count)
+        print('uploading %s (%s)' % (name, retry_count))
         retry_count += 1
         try:
             result = flickr.upload(filename=file, is_public=0, is_family=1, is_friend=1, tags=tags, format='xmlnode')
         except Exception as e:
-            print e
+            print(e)
             continue
         return result['stat'] == 'ok'
     return False
 
 def upload_dir(directory, backup, tags):
-    root, dirs, files = os.walk(directory).next()
-    for name in sorted(files):
-        if os.path.splitext(name)[1] in Extensions:
-            whole_file = os.path.join(root, name)
-            this_hash = file_hash(whole_file)
-            if this_hash in hashes:
-                print 'skipping %s, duplicate of %s' % (name, hashes[this_hash])
-                os.rename(whole_file, os.path.join(backup, name))
-            else:
-                hashes[this_hash] = name
-                if upload_file(name, whole_file, tags):
-                    print 'moving %s' % name
+    for root, dirs, files in os.walk(directory):
+        for name in sorted(files):
+            if os.path.splitext(name)[1] in Extensions:
+                whole_file = os.path.join(root, name)
+                this_hash = file_hash(whole_file)
+                if this_hash in hashes:
+                    print('skipping %s, duplicate of %s' % (name, hashes[this_hash]))
                     os.rename(whole_file, os.path.join(backup, name))
+                else:
+                    hashes[this_hash] = name
+                    if upload_file(name, whole_file, tags):
+                        print('moving %s' % name)
+                        os.rename(whole_file, os.path.join(backup, name))
+        break
 
 def scan_backup(directory):
-    root, dirs, files = os.walk(directory).next()
-    for name in sorted(files):
-        whole_file = os.path.join(root, name)
-        this_hash = file_hash(whole_file)
-        hashes[this_hash] = name
+    for root, dirs, files in os.walk(directory):
+        for name in sorted(files):
+            whole_file = os.path.join(root, name)
+            this_hash = file_hash(whole_file)
+            hashes[this_hash] = name
+        break
 
 def load_hash_repo(filename):
     for line in open(filename):
@@ -77,7 +86,7 @@ def load_hash_repo(filename):
         hashes[splits[0]] = splits[-1]
 
 if not args.api_key or not args.api_secret:
-    print 'must include api key and secret'
+    print('must include api key and secret')
     exit(1)
 
 flickr = flickrapi.FlickrAPI(args.api_key, args.api_secret)
@@ -88,7 +97,7 @@ backup = args.backup_directory
 tags = args.tags if args.tags else ''
 
 if directory == '' or backup == '':
-    print 'include upload and backup directories'
+    print('include upload and backup directories')
     exit(1)
 
 if not os.path.exists(backup):
